eb5da4c704cf1036458c172d68d3f55d6cda907c
ElPotato <marcinbieszke@gmail.com>
2021-01-17 11:12:29 +0000

    move parser into sparated function

--- shell.go
+++ shell.go
@@ -21,14 +21,18 @@ func main() {
 		}
 		output = append(output, input)
 	}
+	ready := Parse(output)
+	fmt.Printf(ready)
+}
 
-	r, _ := regexp.Compile("\t[0-9a-fA-F]+")
-	matched := r.FindAllString(string(output), -1)
+func Parse(input []rune) string {
 	var buffer bytes.Buffer
+	r, _ := regexp.Compile("\t[0-9a-fA-F]+")
+	matched := r.FindAllString(string(input), -1)
 
 	for _, e := range matched {
 		buffer.WriteString(strings.Trim(e, "\t"))
 	}
 
-	fmt.Printf(buffer.String())
+	return buffer.String()
 }
