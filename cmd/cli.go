9ca313b4f5005d7c14ba6f5b622d03cc9e6f3782
ElPotato <marcinbieszke@gmail.com>
2021-01-18 01:44:08 +0000

    add save input/output to dump file flag/functionality

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,11 +1,38 @@
 package main
 
-import sgo "github.com/ElPotato/shellgo"
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	sgo "github.com/ElPotato/shellgo"
+	"io/ioutil"
+)
 
 func main() {
+	dumpPath := flag.String("dump", "", "path for input/output data dump")
+	flag.Parse()
+
 	data := sgo.STDINReader()
 	ready := sgo.Parse(string(data))
 
 	fmt.Printf(ready)
+
+	if *dumpPath != "" {
+		err := saveInFile(*dumpPath, ready, string(data))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func saveInFile(path string, dataOut, dataIn string) error {
+	errIn := ioutil.WriteFile(path+".in", []byte(dataIn), 0644)
+	if errIn != nil {
+		return errIn
+	}
+
+	errOut := ioutil.WriteFile(path+".out", []byte(dataOut), 0644)
+	if errOut != nil {
+		return errOut
+	}
+	return nil
 }
